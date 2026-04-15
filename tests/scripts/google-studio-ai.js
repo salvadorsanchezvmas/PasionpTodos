a1d6879a8f2d01adf3edb2bb2b07a2cfbccae1c4
jcarloshg <carlosj12336@gmail.com>
2026-04-15 13:38:46 -0600

    save all

--- tests/scripts/google-studio-ai.js
+++ tests/scripts/google-studio-ai.js
@@ -1,5 +1,6 @@
 // To run this code you need to install the following dependencies:
-// npm install @google/genai mime dotenv
+// npm install @google/genai mime
+// npm install -D @types/node
 
 import { config } from "dotenv";
 config({ path: ".env.dev" });
@@ -10,30 +11,40 @@ import IMAGE_BASE64 from "./image.js";
 const { GEMINI_API_KEY } = process.env;
 console.log(`GEMINI_API_KEY: `, GEMINI_API_KEY);
 
-const { GoogleGenAI, ThinkingLevel } = await import("@google/genai");
+const { GoogleGenAI, ThinkingLevel, Type } = await import("@google/genai");
 
 async function main() {
   const ai = new GoogleGenAI({
-    apiKey: process.env["GEMINI_API_KEY"],
+    apiKey: process.env['GEMINI_API_KEY'],
   });
   const config = {
     thinkingConfig: {
-      thinkingLevel: ThinkingLevel.MINIMAL,
+      thinkingLevel: ThinkingLevel.LOW,
+    },
+    responseMimeType: 'application/json',
+    responseSchema: {
+      type: Type.OBJECT,
+      required: ["is_valid"],
+      properties: {
+        is_valid: {
+          type: Type.BOOLEAN,
+        },
+      },
     },
   };
-  const model = "gemini-3.1-flash-lite-preview";
+  const model = 'gemini-3.1-flash-lite-preview';
   const contents = [
     {
-      role: "user",
+      role: 'user',
       parts: [
         {
-          text: CHICKEN_CLASSIFIER_PROMPT,
+          inlineData: {
+            data: IMAGE_BASE64,
+            mimeType: 'image/jpeg',
+          },
         },
         {
-          inlineData: {
-            mimeType: "image/jpeg",
-            data: IMAGE_BASE64,
-          },
+          text: CHICKEN_CLASSIFIER_PROMPT,
         },
       ],
     },
