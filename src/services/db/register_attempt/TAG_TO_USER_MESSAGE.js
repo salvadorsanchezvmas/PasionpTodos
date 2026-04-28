42190de8bb3fec0e9b3786db162382461c3edaf2
jcarloshg <carlosj12336@gmail.com>
2026-04-28 01:35:32 -0600

    add tag message for valid image

--- src/services/db/register_attempt/TAG_TO_USER_MESSAGE.js
+++ src/services/db/register_attempt/TAG_TO_USER_MESSAGE.js
@@ -2,6 +2,8 @@
  * Mapping of validation tags to user-friendly messages in Spanish
  */
 export const TAG_TO_USER_MESSAGE = {
+  VALID_IMAGE: 
+    "¡Imagen aprobada! Cumple perfectamente con todos nuestros lineamientos. Gracias por compartir tu foto de pollo con nosotros.",
   NOT_FOOD_OR_CHICKEN:
     "La imagen debe mostrar alimentos o pollo; no se aceptarán imágenes que no estén relacionadas con comida.",
   STOCK_OR_INTERNET_IMAGERY:
